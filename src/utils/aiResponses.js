2c51b177f8b0af1be386fc1acf0f8dd2416a4342
Rafa Mendes <willyanrossanelliwress2703@gmail.com>
2025-11-12 16:51:12 -0300

    Create aiResponses.js

---
+++ src/utils/aiResponses.js
@@ -0,0 +1,7 @@
+export const aiResponses = [
+  "Entendido! Vou verificar isso agora.",
+  "Posso ajudar com isso. Me envie mais detalhes.",
+  "Obrigado pelo contato. Em até 5 minutos resolvo.",
+  "Já abri um chamado interno para análise.",
+  "Seu ticket foi priorizado. Aguarde atualização."
+];
