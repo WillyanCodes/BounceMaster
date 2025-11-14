4b7080961ae7c0bc6f1e9ec4db4e91094cf417b7
Rafa Mendes <willyanrossanelliwress2703@gmail.com>
2025-11-14 19:24:21 -0300

    Delete js directory

--- js/data.js
+++
@@ -1,8 +0,0 @@
-// DADOS GLOBAIS
-let clientes = JSON.parse(localStorage.getItem('bounce_clientes')) || [];
-
-// SALVAR AUTOMÁTICO
-function salvar() {
-  localStorage.setItem('bounce_clientes', JSON.stringify(clientes));
-  atualizarTudo();
-}
